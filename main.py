c62c52ba61f5b2b1fabca4c771751f2a882489e4
platinum-valley <tamishin00@gmail.com>
2023-12-20 01:07:56 +0900

    [fix] importの追加

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import time
+
+from src.bettor import OptimizeTansyoBettor
+from src.load_pred import PredLoader
+from src.notify import Notifier
+from src.scraper import OddsScraper
+
+
+def notify_bet():
+    # Google Driveの予測JSONファイルから予測を取得
+
+    # 現在時刻と予測ファイルの時間の開催時間が近いレースがあれば
+
+    #
+    pass
+
+
 def main():
     pass
 
