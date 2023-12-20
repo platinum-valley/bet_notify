1e9ed55b7f7041b74458374ac00caaa3160f7766
platinum-valley <tamishin00@gmail.com>
2023-12-21 01:36:05 +0900

    [add] Google Drive上のJSONファイルを読む機能の追加

---
+++ src/read_google_drive_json.py
@@ -0,0 +1,74 @@
+import io
+import json
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+
+class GoogleDriveJsonReader:
+    _SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+
+    def __init__(self, credentials_json_path, token_json_path, json_path):
+        self._creds = self._set_credential(credentials_json_path, token_json_path)
+        self._json = self._read_json(json_path)
+
+    def _set_credential(self, credentials_json_path: str, token_json_path: str):
+        creds = None
+
+        if os.path.exists(token_json_path):
+            creds = Credentials.from_authorized_user_file(token_json_path)
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    credentials_json_path, self._SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+        return creds
+
+    def _read_json(self, json_path):
+        # Drive APIクライアントを初期化
+        service = build("drive", "v3", credentials=self._creds)
+
+        # ファイル名で検索
+        query = f"name = '{json_path}'"
+        response = service.files().list(q=query).execute()
+        files = response.get("files", [])
+
+        # ファイルが存在するか
+        if not files:
+            raise FileNotFoundError(f"パスにファイルが見つかりません. {json_path}")
+
+        file_id = files[0]["id"]
+
+        # ファイルをダウンロード
+        request = service.files().get_media(fileId=file_id)
+        file_io = io.ByteIO()
+        downloader = MediaIoBaseDownload(file_io, request)
+
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+
+        file_io.seek(0)
+
+        json_content = json.load(file_io)
+
+        return json_content
+
+    @property
+    def json(self):
+        return self._json
+
+    @property
+    def creds(self):
+        return self._creds
