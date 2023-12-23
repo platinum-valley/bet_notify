6b4e8f849adc3b4f3059c090fc35038e09cd56bd
platinum-valley <tamishin00@gmail.com>
2023-12-23 20:27:09 +0900

    [fix]  ライブラリの追加

--- src/read_google_drive_json.py
+++ src/read_google_drive_json.py
@@ -1,5 +1,6 @@
 import io
 import json
+import os
 import os.path
 
 from google.auth.transport.requests import Request
@@ -27,18 +28,6 @@ class GoogleDriveJsonReader:
             creds = Credentials.from_authorized_user_info(
                 os.environ.get("GOOGLE_DRIVE_TOKEN")
             )
-
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    credentials_json_path, self._SCOPES
-                )
-                creds = flow.run_local_server(port=0)
-            with open("token.json", "w") as token:
-                token.write(creds.to_json())
-
         return creds
 
     def _read_json(self, json_path):
